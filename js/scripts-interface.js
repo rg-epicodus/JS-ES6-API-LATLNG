70664b7f419576f0420c518b60f633def7f9957c
Rich Garrick <richg341@gmail.com>
2017-09-17 18:14:21 -0700

    initialize map and log locations

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,9 +1,20 @@
 import { scripts } from "./../js/scripts.js";
 
+
 $(document).ready(function() {
+  scripts.initialize();
 
 
 
+
+$('#userInputLocation').submit(function(evt) {
+  evt.preventDefault();
+  let location = $("input[name='userInputLocation']").val();
+  $("input").val("");
+
+  console.log("Location submitted " + location);
+  scripts.codeAddress(location);
+});
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
+export let scripts = {
 
-  function initialize() {
-      var geocoder;
-      var map;
-      geocoder = new google.maps.Geocoder();
-      var latlng = new google.maps.LatLng(45.512794, -122.679565);
-      var myOptions = {
-        zoom: 10,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      map = new google.maps.Map(document.getElementById("map_canvas"),
-          myOptions);
-    }
+  initialize: function() {
+    let latlng = new google.maps.LatLng(45.512794, -122.679565);
+    let myOptions = {
+      zoom: 10,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    let map = new google.maps.Map(document.getElementById("map_canvas"),
+    myOptions);
+    console.log("map initialized");
+    let geocoder = new google.maps.Geocoder();
+  },
 
-    function codeAddress() {
-      var sAddress = document.getElementById("inputTextAddress").value;
-      geocoder.geocode( { 'address': sAddress}, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-              map.setCenter(results[0].geometry.location);
-              var marker = new google.maps.Marker({
-                  map: map,
-                  position: results[0].geometry.location
-              });
-          } else {
-              alert("Geocode was not successful for the following reason: " + status);
-          }
-        });
-    }
+  codeAddress: function(geocoder, location, resultsMap) {
+
+    // let map = new google.maps.Map(document.getElementById("map_canvas2"));
+    let sAddress = location;
+    geocoder.geocode( { 'address': sAddress}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          resultsMap.setCenter(results[0].geometry.location);
+          let marker = new google.maps.Marker({
+            map: resultsMap,
+            position: results[0].geometry.location
+          });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
+
+};
